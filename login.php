49b9707e355eb1bd4b974b07f17f2a171dd3a2bf
Tiago Izaias <59627733+tiagoiziaas@users.noreply.github.com>
2024-04-30 19:43:20 -0300

    Meu login connecta

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+include('index.html');
+
+    $pdo = new PDO('mysql:host=localhost;dbname=cadastro','root','');
+    $email = @$_POST['email'];
+    $pass = @$_POST['pass'];
+   
+    $sql = $pdo->prepare("SELECT * FROM `clientes` WHERE Email=? AND Password=? ");
+
+    $sql->execute(array($email,$pass));    
+    $info =  $sql->fetchAll();
+
+    if($info){
+        echo 'inserido';
+        header('Location:https://www.google.com');
+        
+    }   
+    
+
+    
+?>
